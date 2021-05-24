b3e3aeb357792eba9860ef3d6841ef38bf471924
Arup Kumar Gupta <arup.shibai@gmail.com>
2021-05-25 00:40:06 +0530

    removed dist

--- dist/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.connection = void 0;
-var mongoose_1 = require("mongoose");
-exports.default = (function () {
-    return mongoose_1.connect("mongodb+srv://" + process.env.MONGO_USERNAME + ":" + process.env.MONGO_PASSWORD + "@" + process.env.MONGO_HOST + "/" + process.env.DB_NAME + "?retryWrites=true&w=majority");
-});
-//# sourceMappingURL=db.js.map
